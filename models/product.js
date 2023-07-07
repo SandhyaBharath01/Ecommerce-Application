622ba691a9c5f313f2417d324d03076c4368d7ea
SandhyaBharath01 <sandhya.pujari26@gmail.com>
2023-07-07 21:53:48 +0530

    Creating the Users table

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product{
-  constructor(title,price, description, imageUrl, id){
+  constructor(title,price, description, imageUrl, id, userId){
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id =new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id): null;
+    this.userId = userId;
   }
   save() {
     const db = getDb();
@@ -18,7 +19,7 @@ class Product{
     .collection("products")
     .updateOne({_id: this._id}, {$set: this});
     }else{
-      dpOp = db 
+      dbOp = db 
       .collection('products')
       .insertOne(this)
     }
